64a9804f6c8d689c357927c26c6e8059eb17c44a
Ricardo Alvarado <a20190057@pucp.edu.pe>
2023-06-17 15:11:17 -0500

    He puesto solamente la base de datos y el txt aquí

--- Chimchar_LAB8/src/main/java/com/example/chimchar_lab8/controllers/LoginServlet.java
+++
@@ -1,4 +0,0 @@
-package com.example.chimchar_lab8.controllers;
-
-public class LoginServlet {
-}
--- Chimchar_LAB8/src/main/java/com/example/chimchar_lab8/models/daos/Usuarios/StatusDao.java
+++
@@ -1,6 +0,0 @@
-package com.example.chimchar_lab8.models.daos.Usuarios;
-
-import com.example.chimchar_lab8.models.daos.BaseDao;
-
-public class StatusDao extends BaseDao {
-}
